157e288180ae16229fa07b6ce25b09405b84fe66
Billy Vlachos <williamcaminiti@live.com>
2019-07-13 18:45:30 +0200

    Implement configuration file and add additional dev packages

--- config.js
+++ config.js
@@ -1,7 +1,18 @@
+// Define configuration variables
+const connectionString = process.env.DATABASE_URL ||
+    'postgres://postgres:secret@localhost:5432/postgres';
+const port = process.env.PORT || 5000;
+const host = process.env.HOST || 'localhost';
+
+// Construct the url
+const url = (process.env.NODE_ENV === 'production' ?
+    host : `${host}:${port}`);
+
 // Export the configuration module
-module.exports = () => ({
-    appName: 'express-sse-chat-example',
-    connectionString: process.env.DATABASE_URL || 'postgres://postgres:secret@localhost:5432/postgres',
-    port: process.env.PORT || 5000,
-    host: process.env.HOST || 'localhost',
+module.exports = ({
+    name: 'express-sse-chat-example',
+    connectionString,
+    port,
+    host,
+    url
 });
