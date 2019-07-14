7c0db80818f7a0b816407271390aa7edc5242cdc
Billy Vlachos <williamcaminiti@live.com>
2019-07-14 13:43:25 +0200

    Implement a jwt secret

--- config.js
+++ config.js
@@ -8,11 +8,15 @@ const host = process.env.HOST || 'localhost';
 const url = (process.env.NODE_ENV === 'production' ?
     host : `${host}:${port}`);
 
+// Define a secret for the jwt token
+const jwtSecret = process.env.JWT_SECRET || '(*FJKDOA#&U*(&u89e7fiUJ89vEIJ#89fIJGoy89erUG(*WRE)';
+
 // Export the configuration module
 module.exports = ({
     name: 'express-sse-chat-example',
     connectionString,
     port,
     host,
-    url
+    url,
+    jwtSecret
 });
